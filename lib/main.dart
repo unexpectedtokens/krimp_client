2ca2175017673d768acee55d7edcec9571c84f76
Daniel de Jong <dtronicdevs@icloud.com>
2022-01-01 12:30:25 +0100

    photo edit functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:krimp/firebase_options.dart';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -19,14 +21,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see th// application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,15 +31,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,7 +39,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Offset> points = [];
-
+  ui.Image? background;
   void _printLoc(TapDownDetails dets) {
     var newPoints = [...points];
     newPoints.add(dets.localPosition);
@@ -62,72 +47,131 @@ class _MyHomePageState extends State<MyHomePage> {
       points = newPoints;
     });
   }
+
+  void setBackground() async {
+    var _picker = ImagePicker();
+    var image = await _picker.pickImage(source: ImageSource.camera);
+    if (image == null) {
+      return;
+    }
+    var fileL = File(image.path);
+    ui.decodeImageFromList(fileL.readAsBytesSync(), (res) {
+      setState(() {
+        background = res;
+      });
+    });
+  }
+
+  void saveImage() async {
+    ui.PictureRecorder rec = ui.PictureRecorder();
+    Canvas canvas = Canvas(rec);
+    BackgroundPaint painter =
+        BackgroundPaint(points: points, image: background!);
+    var size = context.size;
+    painter.paint(canvas, size!);
+    var image = await rec
+        .endRecording()
+        .toImage(size.width.floor(), size.height.floor());
+    var dat = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (builder) => Thingy(
+              file: dat!,
+            )));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setBackground();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        body: SafeArea(
-          child: GestureDetector(
-            onTapDown: _printLoc,
-            child: Container(
-                color: Colors.black54,
-                height: MediaQuery.of(context).size.height,
-
-                width: MediaQuery.of(context).size.width,
-                child: CustomPaint(
-                  painter: BackgroundPaint(
-                    points: points,
-                  ),
+      body: SafeArea(
+          child: background != null
+              ? GestureDetector(
+                  onTapDown: _printLoc,
+                  child: Container(
+                      color: Colors.black54,
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: CustomPaint(
+                        painter:
+                            BackgroundPaint(points: points, image: background!),
+                      )),
                 )
-            ),
-          ),
-        ),
+              : const Center(
+                  child: CircularProgressIndicator(color: Colors.redAccent))),
       floatingActionButton: FloatingActionButton(
-        child: const Text("Clear"),
-        onPressed: (){
-          setState(() {
-            points = [];
-          });
+        child: const Text("Save"),
+        onPressed: () {
+          saveImage();
         },
       ),
-
     );
   }
 }
 
-
 class BackgroundPaint extends CustomPainter {
   final List<Offset> points;
+  final ui.Image image;
   BackgroundPaint({
-    required this.points
+    required this.points,
+    required this.image,
   });
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
-    // final height = size.height;
-    // final width = size.width;
-
+    paintImage(
+      canvas: canvas,
+      rect: Rect.fromLTRB(0, 0, size.width, size.height),
+      image: image,
+      fit: BoxFit.cover,
+      invertColors: true,
+    );
     Paint dotPaint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.white70
-      ..strokeWidth = 40;
-
+      ..style = PaintingStyle.stroke
+      ..color = Colors.white54
+      ..strokeWidth = 2;
 
     Paint linePaint = Paint()
       ..style = PaintingStyle.fill
       ..strokeWidth = 2
-    ..color = Colors.redAccent;
+      ..color = Colors.redAccent;
 
+    // Paint imagePaint = Paint();
 
-    for (var x = 0; x < points.length; x++){
-      canvas.drawCircle(points[x], 15, dotPaint);
-      if (x < points.length - 1){
-        canvas.drawLine(points[x], points[x + 1], linePaint);
+    // canvas.drawImage(image, Offset.zero, imagePaint);
+
+    for (var x = 0; x < points.length; x++) {
+      canvas.drawCircle(points[x], 20, dotPaint);
+      if (x < points.length - 1) {
+        canvas.drawLine(
+          points[x],
+          points[x + 1],
+          linePaint,
+        );
       }
     }
-    print(points);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+  bool shouldRepaint(BackgroundPaint oldDelegate) =>
+      oldDelegate.points != points;
+}
+
+class Thingy extends StatelessWidget {
+  final ByteData file;
+
+  const Thingy({Key? key, required this.file}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.memory(file.buffer.asUint8List()),
+      ),
+    );
+  }
 }
